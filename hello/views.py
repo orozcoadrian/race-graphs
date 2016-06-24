6eaeb142695f375b55d67f2063985f876167b77e
orozcoadrian <orozcoadrian@gmail.com>
2016-06-24 18:31:43 -0400

    a change

--- hello/views.py
+++ hello/views.py
@@ -7,10 +7,10 @@ import requests
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    #return render(request, 'index.html') some random change
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return render(request, 'index.html')
+    # r = requests.get('http://httpbin.org/status/418')
+    # print r.text
+    # return HttpResponse('<pre>' + r.text + '</pre>')
 
 
 def db(request):
